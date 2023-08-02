da9232383a5504bf6fd207a54c282cbef7c62959
ankitj1996 <ankit.jain@quokkalabs.com>
2023-08-02 16:46:34 +0530

    created blog app

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.pexels.com']
+        domains: ["images.pexels.com"],
+        eslint: {
+            // Warning: This allows production builds to successfully complete even if
+            // your project has ESLint errors.
+            ignoreDuringBuilds: true,
+        },
     }
 }
 
